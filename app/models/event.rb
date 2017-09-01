6614e2dabcb12d61076c1632b04d089ca5fbf50f
Kyle Stengline <kyle.stengline@gmail.com>
2017-08-31 23:38:47 -0700

    refactor event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,6 @@ class Event < ApplicationRecord
   validates_presence_of :name, :time, :duration_minutes
 
   belongs_to :location
-
   has_many :events_users, dependent: :destroy
   has_many :users, through: :events_users
   has_many :timeslots, dependent: :destroy
